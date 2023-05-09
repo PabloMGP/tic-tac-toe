5c0a5b66ee86f594f93c28da5c1c4cff12dff5fa
Pablo MGP <pgarciacardone@gmail.com>
2023-05-09 23:28:34 +0100

    Refactor game code for improved functionality
    - Adding a gameOver property to the player objects to keep track of
    the game state.
    - Creating variables for the human and AI game mode buttons to allow for
    the implementation of the AI mode in future.
    - Adding a resetGrid function to clear the game board when the reset
    button is clicked.

--- app.js
+++ app.js
@@ -7,16 +7,20 @@ function players(player1, player2) {
     player1: {
       name: player1,
       choice: '',
+      gameOver: false,
     },
     player2: {
       name: player2,
       choice: '',
+      gameOver: false,
     },
   };
 }
 
-// *Temporary* Players Creation
+// *Temporary* Players Creation and Game Mode Buttons Variables
 const { player1, player2 } = players('Player One', 'Player Two');
+const human = document.querySelector('.human');
+const ai = document.querySelector('.ai');
 
 // Gameboard Module
 const gameboard = (function () {
@@ -37,6 +41,42 @@ const gameboard = (function () {
     throw new Error('Error at Game Flow Tracker');
   }
 
+  function resetGrid() {
+    const icons = document.querySelectorAll('.iconImg');
+    icons.forEach((element) => {
+      element.remove();
+    });
+  }
+
+  function reset() {
+    gameboard.board = ['null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null'];
+    player1.choice = '';
+    player1.gameOver = false;
+    player2.choice = '';
+    player2.gameOver = false;
+    gameboard.gameState.state = 1;
+    const playerForm = document.querySelector('.playerOneForm');
+    playerForm.classList.add('hidden');
+
+    resetGrid();
+  }
+
+  // Winning Condition Function
+  function winningCondition(board, choice) {
+    const winConditions = [[0, 1, 2], [0, 3, 6], [1, 4, 7],
+      [3, 4, 5], [6, 7, 8], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+    const isTie = board.every((position) => position !== 'null');
+    const isWinner = winConditions.some((cond) => cond.every((index) => board[index] === choice));
+    if (isWinner) {
+      return true;
+    } if (isTie && !isWinner) {
+      alert('Tie Game!');
+      reset();
+      human.classList.toggle('hidden');
+      ai.classList.toggle('hidden');
+    }
+  }
+
   // Gameboard Interaction / Display
   const gameboardContainer = document.querySelector('.gameboard');
 
@@ -45,24 +85,39 @@ const gameboard = (function () {
       if (choice === 'cross') {
         const crossImg = document.createElement('img');
         crossImg.setAttribute('src', './images/cross.png');
+        crossImg.classList.add('iconImg');
         return crossImg;
       } if (choice === 'circle') {
         const circleImg = document.createElement('img');
         circleImg.setAttribute('src', './images/circle.png');
+        circleImg.classList.add('iconImg');
         return circleImg;
       }
       throw new Error('Error at Gameboard Interaction / Display');
     }
+
     const gameboardElement = event.target;
     const { index } = gameboardElement.dataset;
 
-    if (index !== undefined) {
+    if (index !== undefined && player1.choice !== '') {
       const player = currentPlayer();
       const { choice } = player;
       const icon = createIcon(choice);
 
+      // Data Biding to avoid repeated choices at same array
+      if (gameboard.board[index] !== 'null') {
+        return;
+      }
       gameboardElement.appendChild(icon);
       gameboard.board[index] = choice;
+
+      if (winningCondition(gameboard.board, choice)) {
+        player.gameOver = true;
+        alert(`${player.name} Wins the Game!`);
+        human.classList.toggle('hidden');
+        ai.classList.toggle('hidden');
+        reset();
+      }
     }
   });
   return {
@@ -72,26 +127,6 @@ const gameboard = (function () {
   };
 }());
 
-// Player Two Creation Factory Function
-function createPlayerTwoForm(player) {
-  return function () {
-    const playerTwoForm = document.querySelector('.playerTwoForm');
-    playerTwoForm.classList.toggle('hidden');
-
-    const playerTwoChoice = document.querySelector('select[name="choiceTwo"]');
-    playerTwoChoice.addEventListener('change', () => {
-      const selectedChoice = playerTwoChoice.value;
-      if (selectedChoice === 'cross') {
-        player.choice = 'cross';
-        playerTwoForm.classList.toggle('hidden');
-      } if (selectedChoice === 'circle') {
-        player.choice = 'circle';
-        playerTwoForm.classList.toggle('hidden');
-      }
-    });
-  };
-}
-
 // Game Modes Module - HumanVsAi Pending
 const gameMode = (function () {
   function initHumanVsHuman() {
@@ -99,18 +134,18 @@ const gameMode = (function () {
     playerOneForm.classList.toggle('hidden');
 
     const playerOneChoice = document.querySelector('select[name="choice"]');
-    playerOneChoice.addEventListener('change', () => {
+    playerOneChoice.value = playerOneChoice.options[0].value;
+    playerOneChoice.addEventListener('change', (event) => {
+      event.preventDefault();
       const selectedChoice = playerOneChoice.value;
       if (selectedChoice === 'cross') {
         player1.choice = 'cross';
-        playerOneForm.classList.toggle('hidden');
-        const playerTwoForm = createPlayerTwoForm(player2);
-        return playerTwoForm();
+        player2.choice = 'circle';
+        return playerOneForm.classList.add('hidden');
       } if (selectedChoice === 'circle') {
         player1.choice = 'circle';
-        playerOneForm.classList.toggle('hidden');
-        const playerTwoForm = createPlayerTwoForm(player2);
-        return playerTwoForm();
+        player2.choice = 'cross';
+        return playerOneForm.classList.add('hidden');
       }
       throw new Error('Error at Game Modes Module');
     });
@@ -119,8 +154,9 @@ const gameMode = (function () {
   };
 }());
 
-// Human VS Human Mode
-const human = document.querySelector('.human');
+// Human VS Human and Human Vs AI Mode Buttons
 human.addEventListener('click', () => {
   gameMode.initHumanVsHuman();
+  human.classList.toggle('hidden');
+  ai.classList.toggle('hidden');
 });
